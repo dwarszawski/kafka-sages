2a176be3e8be8646d5da9d1a5ced7a0fa2265d15
Damian Warszawski <damian.warszawski@ing.com>
2020-02-13 00:02:48 +0100

    kafka streams first app

---
+++ 3-kafka-streams/src/main/java/com/sages/app/FirstStreamsApp.java
@@ -0,0 +1,28 @@
+package com.sages.app;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
+public class FirstStreamsApp {
+    public static void main(String[] args) {
+
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "first-streams-app");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        // TODO build stream topology
+
+        // TODO start stream processing
+
+        // TODO add shutdown hooks
+    }
+}
