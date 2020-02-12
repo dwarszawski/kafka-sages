9bfe66f789efd5d34e5dca45e6f973be1b0370ed
Damian Warszawski <damian.warszawski@ing.com>
2020-02-13 00:03:48 +0100

    add solutions for ex3

--- 3-kafka-streams/src/main/java/com/sages/app/FirstStreamsApp.java
+++ 3-kafka-streams/src/main/java/com/sages/app/FirstStreamsApp.java
@@ -3,11 +3,14 @@ package com.sages.app;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 
+import java.util.Arrays;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 public class FirstStreamsApp {
     public static void main(String[] args) {
@@ -19,10 +22,27 @@ public class FirstStreamsApp {
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-        // TODO build stream topology
+        StreamsBuilder builder = new StreamsBuilder();
 
-        // TODO start stream processing
+        KStream<String, String> source = builder.stream("input-topic-name");
 
-        // TODO add shutdown hooks
+        final Pattern pattern = Pattern.compile("\\W+");
+        KStream counts  = source
+                .flatMapValues(value-> Arrays.asList(pattern.split(value.toLowerCase())))
+                .map((key, value) -> new KeyValue<Object, Object>(value, value))
+                .filter((key, value) -> (!value.equals("the")))
+                .groupByKey()
+                .count()
+                .mapValues(value->Long.toString(value))
+                .toStream();
+        counts.to("wordcount-output");
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+
+        streams.cleanUp(); // only do this in dev - not in prod
+        streams.start();
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 }
