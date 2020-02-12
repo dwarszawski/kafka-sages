b5998cb054b74c77ee02a2482c7b3d341a90ddc6
Damian Warszawski <damian.warszawski@ing.com>
2020-02-12 23:48:21 +0100

    advanced consumer/producer solutions

--- 2-kafka-java-advanced/src/main/java/com/sages/templates/AdvancedConsumer.java
+++ 2-kafka-java-advanced/src/main/java/com/sages/templates/AdvancedConsumer.java
@@ -23,6 +23,7 @@ public class AdvancedConsumer {
             for (ConsumerRecord<String, String> record : records) {
                 System.out.println("Key: " + record.key() + ", Value: " + record.value());
                 System.out.println("Partition: " + record.partition() + ", Offset:" + record.offset());
+                consumer.commitSync();
             }
         }
     }
@@ -30,17 +31,19 @@ public class AdvancedConsumer {
     public static KafkaConsumer<String, String> createConsumer(String topic) {
 
         String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "kafka-advanced-consumer";
 
         Properties properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-
-        // TODO disable autocommit, read from earliest offset, setup groupId and setup max records to poll
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
-
-        // TODO subscribe to topic
+        consumer.subscribe(Arrays.asList(topic));
 
         return consumer;
 
